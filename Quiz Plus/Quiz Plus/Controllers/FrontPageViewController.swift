768d2044879aeaf6c8bebf3d9b75a90405fb68b5
Sushi-UX <thechoygroup@gmail.com>
2019-07-09 23:59:33 -0400

    Rebuit Front page and Quiz Page
    Rebuit Front page and Quiz Page

--- Quiz Plus/Quiz Plus/Controllers/FrontPageViewController.swift
+++ Quiz Plus/Quiz Plus/Controllers/FrontPageViewController.swift
@@ -10,13 +10,20 @@ import UIKit
 
 class FrontPageViewController: UIViewController {
 
+    @IBAction func StartPress(_ sender: Any) {
+        performSegue(withIdentifier: "startToQuiz", sender: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
     /*
     // MARK: - Navigation
 
--- Quiz Plus/Quiz Plus/Controllers/QuizViewController.swift
+++ Quiz Plus/Quiz Plus/Controllers/QuizViewController.swift
@@ -6,17 +6,50 @@
 //  Copyright © 2019 thechoygroup. All rights reserved.
 //
 
+
+
 import UIKit
 
-class QuizViewController: UIViewController {
 
+class quizCell: UITableViewCell{
+    
+    @IBOutlet weak var answerLabel: UILabel!    
+}
+
+class QuizViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return quizAnswerList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "quizCell") as! quizCell
+        cell.answerLabel.text = quizAnswerList[indexPath.row]
+        return cell
+    }
+    
+    
+    var quizAnswerList = ["answer 1", "answer 2", "answer 3", "answer 4"]
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
+    @IBAction func actionSubmit(_ sender: Any) {
+    }
+    
+    
+    @IBOutlet weak var quizTable: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        quizTable.delegate = self
+        quizTable.dataSource = self
         // Do any additional setup after loading the view.
     }
     
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     /*
     // MARK: - Navigation
 
